23887c7c11abce50665b08e20b3c693cf6b238aa
JewelChidinma <jewelchidinma@gmail.com>
2026-05-14 17:19:52 +0100

    Make phone number unique in user schema

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema(
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  phone: { type: String, required: true },
+  phone: { type: String, required: true, unique: true },
   password: { type: String, required: true, select: false },
   role: { type: String, enum: ['admin', 'driver', 'user'], default: 'user' },
 },
